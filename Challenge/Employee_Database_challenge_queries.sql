60374c0f4cff412f1a5cfee31035824bbe18b309
Your Name <mmrtait@gmail.com>
2022-08-04 16:11:23 -0400

    completed deliverable 2

--- Challenge/Employee_Database_challenge_queries.sql
+++ Challenge/Employee_Database_challenge_queries.sql
@@ -27,3 +27,23 @@ ORDER BY count DESC;
 
 SELECT * FROM retiring_titles;
 
+-- Create a Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name, 
+    e.last_name, 
+    e.birth_date, 
+    de.from_date, 
+    de.to_date, 
+    ti.title
+INTO mentorship
+FROM employees AS e
+INNER JOIN dept_emp AS de
+    ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+    ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+    AND (e.birth_date BETWEEN '1965-01-01' AND 
+        '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship;
